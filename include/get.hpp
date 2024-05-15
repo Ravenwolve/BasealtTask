3c033c9ad5c3d2f91d27f81615f8448b6208f3cf
Kirill Sharov <kira.sharov@yandex.ru>
2024-05-15 19:40:53 +0400

    Some unworking commit instead of stash

--- include/get.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-namespace BasealtTask {
-    std::string Get(const std::string& url);
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include <basealttask/get.hpp>
+#include <nlohmann/json.hpp>
 #include <iostream>
-#include "get.hpp"
+#include <algorithm>
+#include <set>
 
-int main() {
-    auto str = ALTRepo::Get("https://rdb.altlinux.org/api/export/branch_binary_packages/p9");
+static const std::string urlHeader = "https://rdb.altlinux.org/api/export/branch_binary_packages/";
+
+void Help() {
+
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1 || argv[1] == "help") {
+        Help();
+    }
+    else if (argc != 3) {
+        std::cout << "It is required to specify 2 branches!\n";
+    }
+    else {
+        std::string firstBranch = argv[1], secondBranch = argv[2];
+        std::string firstPackage = BasealtTask::Get(urlHeader + firstBranch);
+        std::string secondPackage = BasealtTask::Get(urlHeader + secondBranch);
+        nlohmann::json data = nlohmann::json::parse(firstPackage);
+        std::cout << data;
+    }
 }
