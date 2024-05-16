1f5e552d17cb8371f2ddcd424dac66c1e29f0aec
Kirill Sharov <kira.sharov@yandex.ru>
2024-05-16 21:35:14 +0400

    Added outer-left and outer-right sets of input packages.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,24 @@
 #include <iostream>
 #include <algorithm>
 #include <set>
+#include <package.hpp>
 
 static const std::string urlHeader = "https://rdb.altlinux.org/api/export/branch_binary_packages/";
 
 void Help() {
-
+    
 }
+
+// using Iter = std::set<BasealtTask::Package>::iterator;
+// Iter MoreVersionRelease(Iter first1, Iter last1, Iter first2, Iter last2, Iter output_first) {
+//     while (first1 != last1) {
+//         if (first2 == last2)
+//             return std::copy(first1, last1, output_first);
+//         if (first1->name == first2->name && first1->) {
+
+//         }
+//     }
+//  }
 
 int main(int argc, char* argv[]) {
     if (argc == 1 || argv[1] == "help") {
@@ -18,10 +30,20 @@ int main(int argc, char* argv[]) {
         std::cout << "It is required to specify 2 branches!\n";
     }
     else {
+        using namespace nlohmann;
         std::string firstBranch = argv[1], secondBranch = argv[2];
-        std::string firstPackage = BasealtTask::Get(urlHeader + firstBranch);
-        std::string secondPackage = BasealtTask::Get(urlHeader + secondBranch);
-        nlohmann::json data = nlohmann::json::parse(firstPackage);
-        std::cout << data;
+        auto firstSet = json::parse(BasealtTask::Get(urlHeader + firstBranch))["packages"].get<std::set<BasealtTask::Package>>();
+        auto secondSet = json::parse(BasealtTask::Get(urlHeader + secondBranch))["packages"].get<std::set<BasealtTask::Package>>();
+        std::set<BasealtTask::Package> outerLeft, outerRight;
+        std::set_difference(firstSet.begin(), firstSet.end(), secondSet.begin(), secondSet.end(), std::inserter(outerLeft, outerLeft.end()));
+        std::set_difference(secondSet.begin(), secondSet.end(), firstSet.begin(), firstSet.end(), std::inserter(outerRight, outerRight.end()));
+        json result;
+
+        json doc{
+            { "OuterLeft", outerLeft },
+            { "OuterRight", outerRight },
+            //{ "VersionReleaseMoreFromLeft", versionReleaseMoreFromLeft }
+        };
+        std::cout << doc;
     }
 }
