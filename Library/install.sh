6be44edb8b81c58a7be0b1eef0c8a728cb78dc15
Kirill Sharov <kira.sharov@yandex.ru>
2024-05-18 18:50:24 +0400

    Added -pthread flag for compiler.

--- Library/install.sh
+++ Library/install.sh
@@ -1,5 +1,5 @@
 cd build
-sudo cp -a usr /
-sudo touch /etc/ld.so.conf.d/basealttask.conf
-sudo sh -c "echo '/usr/lib64' > /etc/ld.so.conf.d/basealttask.conf"
-sudo ldconfig
+cp -a usr /
+touch /etc/ld.so.conf.d/basealttask.conf
+sh -c "echo '/usr/lib64' > /etc/ld.so.conf.d/basealttask.conf"
+ldconfig
--- Utility/install.sh
+++ Utility/install.sh
@@ -1,2 +1,2 @@
 cd build
-sudo cp -a usr /
+cp -a usr /
